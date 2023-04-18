c0516b44fd7e5e9b26441a01635b89abda3ea6f0
beginwebdevs <125490500+beginwebdevs@users.noreply.github.com>
2023-04-18 13:53:30 +0530

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -15,23 +15,17 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-const whitelist = ['https://vivekf.beginweb.in', 'http://localhost:3000', 'https://vadmin.beginweb.in'];
 
 
-app.use(cors({ credentials: true, origin: (origin, callback) => {
-    if(whitelist.indexOf(origin) !== -1 || !origin){
-        callback(null, true)
-    }else{
-        callback(new Error('Not allowe by cors'))
-    }
-} }));
+
+app.use(cors({ credentials: true, origin: '*' }));
 app.use(express.json({limit: '8mb'}));
 app.use(cookieParser())
 app.use(routes);
 
 
 app.get('/', (req, res) => {
-    res.send('backend update 2');
+    res.send('backend update 3');
 })
 
 
